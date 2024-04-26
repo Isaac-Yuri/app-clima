adfeb558beb8ea31a05e2dad0eaf3f35e7e81f27
Isaac <isaacyuri22@gmail.com>
2024-04-26 10:13:17 -0300

    :sparkles: feat: agora os dados vem da api

---
+++ src/api/index.ts
@@ -0,0 +1,4 @@
+const BASE_URL_API = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "c97ca3146090a7e02611d363106840e0";
+
+export { BASE_URL_API, API_KEY };
--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,11 +1,42 @@
 import { ChangeEvent, useState } from "react";
+import { BASE_URL_API, API_KEY } from "../../api";
 import "./style.css";
 
+interface IWeather {
+  description: string;
+  icon: string;
+  main: string;
+}
+interface IMain {
+  temp: number;
+  humidity: number;
+}
+
+interface IWind {
+  speed: number;
+}
+interface IWeatherData {
+  main: IMain;
+  weather: IWeather[];
+  name: string;
+  wind: IWind;
+}
+
 function Weather() {
-  const [city, setCity] = useState("tacima");
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherDada] = useState<IWeatherData>();
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     setCity(e.target.value);
+  }
+
+  async function handleWeatherData() {
+    const response = await fetch(
+      `${BASE_URL_API}?q=${city}&appid=${API_KEY}&lang=pt_br`
+    );
+    const data = await response.json();
+    setWeatherDada(data);
+    console.log(data);
   }
 
   return (
@@ -16,22 +47,32 @@ function Weather() {
         </div>
 
         <div className="box-button-search">
-          <button id="button-search">Pesquisar</button>
+          <button id="button-search" onClick={handleWeatherData}>
+            Pesquisar
+          </button>
         </div>
       </div>
-      <div className="box-image-weather">
-        <img src="" alt="Ícone do clima" id="image-weather" />
-      </div>
-      <div className="box-temperature">
-        <span id="temperature">30°C</span>
-      </div>
-      <div className="box-name-city">
-        <h1 id="name-city">Tacima</h1>
-      </div>
-      <div className="box-wind-humidity">
-        <div className="box-humidity">Humidity: 50%</div>
-        <div className="box-wind">Wind Speed: 2,76 km/h</div>
-      </div>
+      {weatherData && (
+        <div>
+          <div className="box-image-weather">
+            <img src="" alt="Ícone do clima" id="image-weather" />
+          </div>
+          <div className="box-temperature">
+            <span id="temperature">{(weatherData.main.temp - 273.15).toFixed(2)}°C</span>
+          </div>
+          <div className="box-name-city">
+            <span id="name-city">{weatherData.name}</span>
+          </div>
+          <div className="box-wind-humidity">
+            <div className="box-humidity">
+              Humidity: {weatherData.main.humidity}%
+            </div>
+            <div className="box-wind">
+              Wind Speed: {weatherData.wind.speed} km/h
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
