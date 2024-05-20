0df3cff8e773a44792c6626ecb71e7d38019fa78
Isaac Yuri <isaacyuri22@gmail.com>
2024-05-20 09:35:26 -0300

    :lipstick: feat: estilo do card adicionado

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,6 +1,10 @@
 import { ChangeEvent, useState } from "react";
 import { BASE_URL_API, API_KEY } from "../../api";
 import "./style.css";
+import Map from "/map.svg";
+import Search from "/search.svg";
+import Humidity from "/humidity.svg";
+import Wind from "/wind.svg";
 
 interface IWeather {
   description: string;
@@ -42,33 +46,53 @@ function Weather() {
   return (
     <div className="card-weather">
       <div className="box-search">
-        <div className="box-input">
-          <input onChange={handleChange} type="text" value={city} />
-        </div>
-
-        <div className="box-button-search">
-          <button id="button-search" onClick={handleWeatherData}>
-            Pesquisar
-          </button>
-        </div>
+        <img src={Map} id="icon-map" alt="" />
+        <input
+          onChange={handleChange}
+          placeholder="Digite aqui uma cidade"
+          type="text"
+          value={city}
+          id="input-search"
+        />
+        <button>
+          <img
+            src={Search}
+            id="search-button"
+            alt=""
+            onClick={handleWeatherData}
+          />
+        </button>
       </div>
       {weatherData && (
-        <div>
-          <div className="box-image-weather">
-            <img src={`${weatherData.weather[0].icon}.png`} alt="Ícone do clima" id="image-weather" />
-          </div>
-          <div className="box-temperature">
-            <span id="temperature">{(weatherData.main.temp - 273.15).toFixed(2)}°C</span>
-          </div>
-          <div className="box-name-city">
-            <span id="name-city">{weatherData.name}</span>
+        <div className="box-information">
+          <img
+            src={`${weatherData.weather[0].icon}.png`}
+            alt="Ícone do clima"
+            id="image-weather"
+          />
+          <p id="temperature">
+            {(weatherData.main.temp - 273.15).toFixed(2)}
+            <span>°C</span>
+          </p>
+          <div className="box-name-and-description">
+            <p id="name-city">{weatherData.name}:</p>
+            <p id="description">{weatherData.weather[0].description}</p>
           </div>
           <div className="box-wind-humidity">
             <div className="box-humidity">
-              Umidade: {weatherData.main.humidity}%
+              <img src={Humidity} alt="" />
+              <div className="text">
+                <div>{weatherData.main.humidity}%</div>
+                <p>Umidade</p>
+              </div>
             </div>
+
             <div className="box-wind">
-              Velocidade do vento: {weatherData.wind.speed} km/h
+              <img src={Wind} alt="" />
+              <div className="text">
+                <div>{weatherData.wind.speed} km/h</div>
+                <p>Velocidade do vento</p>
+              </div>
             </div>
           </div>
         </div>
