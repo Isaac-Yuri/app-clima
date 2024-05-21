8c7daed9e47a4f1ca13df0d7d29a4196bd3c4fd1
Isaac Yuri <isaacyuri22@gmail.com>
2024-05-21 10:06:27 -0300

    :sparkles: feat: agora tailwind é usado para estilizar a aplicação

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,6 +1,5 @@
 import { ChangeEvent, useState } from "react";
 import { BASE_URL_API, API_KEY } from "../../api";
-import "./style.css";
 import Map from "/map.svg";
 import Search from "/search.svg";
 import Humidity from "/humidity.svg";
@@ -44,52 +43,62 @@ function Weather() {
   }
 
   return (
-    <div className="card-weather">
-      <div className="box-search">
-        <img src={Map} id="icon-map" alt="" />
+    <div className="border-solid border-2 border-[#ffffff33] flex flex-col justify-center items-center p-4 rounded-2xl text-white backdrop-blur-lg bg-[#ffffff19] min-w-96 min-h-16">
+      <div className="relative w-full h-full flex items-center">
+        <img
+          className="absolute left-3 w-6 cursor-pointer z-10"
+          src={Map}
+          id="icon-map"
+          alt=""
+        />
         <input
           onChange={handleChange}
           placeholder="Digite aqui uma cidade"
           type="text"
           value={city}
-          id="input-search"
+          className="w-full h-full bg-transparent border-2 border-white border-opacity-10 outline-none rounded-lg text-lg text-white font-medium uppercase p-4 px-10 placeholder-white placeholder-capitalize"
         />
-        <button>
+        <button className="flex justify-center items-center absolute right-0 w-10 h-full bg-transparent border-none outline-none text-2xl px-[30px] pl-[5px] cursor-pointer box-border">
           <img
+            className="absolute h-6 w-6"
             src={Search}
-            id="search-button"
             alt=""
             onClick={handleWeatherData}
           />
         </button>
       </div>
       {weatherData && (
-        <div className="box-information">
+        <div className="flex flex-col justify-center items-center my-10">
           <img
             src={`${weatherData.weather[0].icon}.png`}
             alt="Ícone do clima"
-            id="image-weather"
+            className="w-64"
           />
-          <p id="temperature">
+          <p className="relative leading-3 font-bold text-5xl my-7">
             {(weatherData.main.temp - 273.15).toFixed(2)}
-            <span>°C</span>
+            <span
+              className="absolute text-3xl mx-1 bottom-1
+            "
+            >
+              °C
+            </span>
           </p>
-          <div className="box-name-and-description">
-            <p id="name-city">{weatherData.name}:</p>
-            <p id="description">{weatherData.weather[0].description}</p>
+          <div className="flex gap-6 capitalize text-2xl font-semibold">
+            <p>{weatherData.name}:</p>
+            <p>{weatherData.weather[0].description}</p>
           </div>
-          <div className="box-wind-humidity">
-            <div className="box-humidity">
-              <img src={Humidity} alt="" />
-              <div className="text">
+          <div className="flex w-full justify-around my-5 gap-5">
+            <div className="flex justify-center items-center gap-3">
+              <img className="w-14" src={Humidity} alt="" />
+              <div className="leading-6">
                 <div>{weatherData.main.humidity}%</div>
                 <p>Umidade</p>
               </div>
             </div>
 
-            <div className="box-wind">
-              <img src={Wind} alt="" />
-              <div className="text">
+            <div className="flex justify-center items-center gap-3">
+              <img className="w-14" src={Wind} alt="" />
+              <div className="leading-6">
                 <div>{weatherData.wind.speed} km/h</div>
                 <p>Velocidade do vento</p>
               </div>
