381828a0757ed8a649be9b25805c366a06e06f56
Isaac Yuri <isaacyuri22@gmail.com>
2024-12-21 08:37:11 -0300

    :recycle: refactor: mudanças na semântica da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import Weather from "./components/Weather";
+import Header from "./components/NavBar";
 
 function App() {
   return (
-    <div className="min-h-96 md:min-h-0">
-      <Weather />
+    <div className="flex justify-center items-center min-h-screen">
+      <header className="absolute top-0 w-100">
+        <Header />
+      </header>
+
+      <main className="min-h-150 md:min-h-0">
+        <Weather />
+      </main>
     </div>
   );
 }
